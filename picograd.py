fbc87b3462b7b25724728d94b206028ef38e4f36
breandan <bre@ndan.co>
2021-05-29 14:32:46 -0400

    add todo

--- picograd.py
+++ picograd.py
@@ -3,7 +3,8 @@ from functools import reduce
 
 class Var:
   def __init__(self, val, grad_fn=lambda: []):
-    self.v, self.grad_fn, self.grads = val, grad_fn, self.grad(grad_fn=grad_fn)
+    self.v, self.grad_fn = val, grad_fn
+    self.grads = self.grad()
 
   def __add__(self, other):
     return Var(val=self.v + other.v, grad_fn=lambda: [(self, 1.0), (other, 1.0)])
@@ -11,9 +12,9 @@ class Var:
   def __mul__(self, other):
     return Var(val=self.v * other.v, grad_fn=lambda: [(self, other.v), (other, self.v)])
 
-  def grad(self, *vars, grad_fn=lambda: [], bp=1.0):  # TODO: incrementalize?
+  def grad(self, *vars, bp=1.0):  # TODO: incrementalize?
     return reduce(lambda a, b: {k: a.get(k, 0) + b.get(k, 0) for k in set(a) | set(b)},
-                  [input.grad(*vars, grad_fn=input.grad_fn, bp=val * bp) for (input, val) in grad_fn()],
+                  [input.grad(*vars, bp=val * bp) for (input, val) in self.grad_fn()],
                   {self: bp}) if not vars else {v: self.grads[v] for v in vars}
 
 
@@ -21,6 +22,6 @@ x = Var(1.)
 y = Var(2.)
 f = x * x + y * y * y
 print(f.v)  # 9.0
-grads = f.grad(x, y)
+grads = f.grad(x, y)  # TODO: possible to support f.grad(x=1., y=2.)?
 print(grads[x])  # 2.0
 print(grads[y])  # 12.0
