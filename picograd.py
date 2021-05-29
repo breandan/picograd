267a3a560916f75725ebad8ecc15ac1fe3bb5534
breandan <bre@ndan.co>
2021-05-29 15:00:41 -0400

    add type hints

--- picograd.py
+++ picograd.py
@@ -7,13 +7,13 @@ class Var:
     self.v, self.grad_fn = val, grad_fn
     self.grads = self.grad()
 
-  def __add__(self: Var, other: Var):
+  def __add__(self, other: Var) -> Var:
     return Var(val=self.v + other.v, grad_fn=lambda: [(self, 1.0), (other, 1.0)])
 
-  def __mul__(self: Var, other: Var):
+  def __mul__(self, other: Var) -> Var:
     return Var(val=self.v * other.v, grad_fn=lambda: [(self, other.v), (other, self.v)])
 
-  def grad(self: Var, *vars: Var, bp=1.0):  # TODO: incrementalize?
+  def grad(self, *vars: Var, bp: float = 1.0) -> dict[Var, float]:  # TODO: incrementalize?
     return reduce(lambda a, b: {k: a.get(k, 0) + b.get(k, 0) for k in set(a) | set(b)},
                   [input.grad(*vars, bp=val * bp) for (input, val) in self.grad_fn()],
                   {self: bp}) if not vars else {v: self.grads[v] for v in vars}
