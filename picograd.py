9b6c75dede791a33bc3fd2630054a65a0c822960
breandan <bre@ndan.co>
2021-05-29 14:52:46 -0400

    add type annotations

--- picograd.py
+++ picograd.py
@@ -1,18 +1,19 @@
+from __future__ import annotations
 from functools import reduce
 
 
 class Var:
-  def __init__(self, val, grad_fn=lambda: []):
+  def __init__(self, val: float, grad_fn: callable = lambda: []):
     self.v, self.grad_fn = val, grad_fn
     self.grads = self.grad()
 
-  def __add__(self, other):
+  def __add__(self: Var, other: Var):
     return Var(val=self.v + other.v, grad_fn=lambda: [(self, 1.0), (other, 1.0)])
 
-  def __mul__(self, other):
+  def __mul__(self: Var, other: Var):
     return Var(val=self.v * other.v, grad_fn=lambda: [(self, other.v), (other, self.v)])
 
-  def grad(self, *vars, bp=1.0):  # TODO: incrementalize?
+  def grad(self: Var, *vars: Var, bp=1.0):  # TODO: incrementalize?
     return reduce(lambda a, b: {k: a.get(k, 0) + b.get(k, 0) for k in set(a) | set(b)},
                   [input.grad(*vars, bp=val * bp) for (input, val) in self.grad_fn()],
                   {self: bp}) if not vars else {v: self.grads[v] for v in vars}
