dd8431847c5c8ed209bffc5dd0c75122604662e5
breandan <bre@ndan.co>
2021-05-29 15:37:31 -0400

    convert numbers to floats

--- picograd.py
+++ picograd.py
@@ -1,20 +1,21 @@
 from __future__ import annotations
 
+from numbers import Number
 from functools import reduce
 from typing import Union
 
 
 class Var:
-  def __init__(self, val: float, grad_fn: callable[[], List[Tuple[Var, float]]] = lambda: []):
-    self.v, self.grad_fn = val, grad_fn
+  def __init__(self, val: Number, grad_fn: callable[[], List[Tuple[Var, float]]] = lambda: []):
+    self.v, self.grad_fn = float(val), grad_fn
     self.grads = self.grad()
 
-  def __add__(self, val: Union[Var, int]) -> Var:
-    other = Var(val) if type(val) is float else val
+  def __add__(self, val: Union[Var, float]) -> Var:
+    other = Var(val) if isinstance(val, Number) else val
     return Var(val=self.v + other.v, grad_fn=lambda: [(self, 1.0), (other, 1.0)])
 
-  def __mul__(self, val: Union[Var, int]) -> Var:
-    other = Var(val) if type(val) is float else val
+  def __mul__(self, val: Union[Var, float]) -> Var:
+    other = Var(val) if isinstance(val, Number) else val
     return Var(val=self.v * other.v, grad_fn=lambda: [(self, other.v), (other, self.v)])
 
   def grad(self, *vars: Var, bp: float = 1.0) -> dict[Var, float]:  # TODO: incrementalize?
@@ -23,10 +24,10 @@ class Var:
                   {self: bp}) if not vars else {v: self.grads[v] for v in vars}
 
 
-x = Var(1.)
-y = Var(2.)
-f = x * x + y * y * y * 1.0
+x = Var(1)
+y = Var(2)
+f = (x * x * 3 + y * y * y) * 2
 print(f.v)  # 9.0
-grads = f.grad(x, y)  # TODO: possible to support f.grad(x=1., y=2.)?
+grads = f.grad(x, y)  # TODO: possible to support f.grad(x=1, y=2)?
 print(grads[x])  # 2.0
 print(grads[y])  # 12.0
